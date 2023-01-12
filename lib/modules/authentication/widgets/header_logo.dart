433e1eb51d3424f2a750743beb74ab48e519a1c5
feriwnarta <feriwnarta26@gmail.com>
2023-01-12 14:44:23 +0700

    update before precache image

--- lib/modules/authentication/widgets/header_logo.dart
+++ lib/modules/authentication/widgets/header_logo.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class HeaderLogo extends StatelessWidget {
+class HeaderLogo extends StatefulWidget {
   const HeaderLogo({
     Key key,
   }) : super(key: key);
+
+  @override
+  State<HeaderLogo> createState() => _HeaderLogoState();
+}
+
+class _HeaderLogoState extends State<HeaderLogo> {
+  AssetImage image = AssetImage('assets/img/background-login-rw.png');
+
+  @override
+  void didChangeDependencies() {
+    precacheImage(image, context);
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +27,7 @@ class HeaderLogo extends StatelessWidget {
       decoration: BoxDecoration(
           borderRadius: BorderRadius.only(bottomLeft: Radius.circular(30)),
           image: DecorationImage(
-            image: AssetImage('assets/img/background-login-rw.png'),
+            image: image,
             alignment: Alignment.center,
             fit: BoxFit.fill,
             repeat: ImageRepeat.noRepeat,
