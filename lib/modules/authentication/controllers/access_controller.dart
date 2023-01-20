83fd3c451a977ecb77e3523bd3c088fc79289f46
feriwnarta <feriwnarta26@gmail.com>
2023-01-20 17:02:27 +0700

    update access hanya untuk access manager

--- lib/modules/authentication/controllers/access_controller.dart
+++ lib/modules/authentication/controllers/access_controller.dart
@@ -1,10 +1,32 @@
 import 'package:get/get.dart';
 
 class AccessController extends GetxController {
+  // access warga
   RxBool statistikPeduliLindungi = true.obs;
+  RxBool peduliLingkungan = true.obs;
+  RxBool statusPeduliLingkungan = true.obs;
+  RxBool statusIpl = true.obs;
+  RxBool informasiWarga = true.obs;
+  RxBool informasiUmum = true.obs;
+  RxBool sosialMedia = true.obs;
+
+  // access estate manager
+  RxBool dashboardEm = false.obs;
+  RxBool tambahAkunEm = false.obs;
+  RxBool statusPeduliLingkunganEm = false.obs;
 
   void accessAsEm() {
     statistikPeduliLindungi = false.obs;
+    peduliLingkungan = false.obs;
+    statusPeduliLingkungan = false.obs;
+    statusIpl = false.obs;
+    informasiWarga = false.obs;
+    informasiUmum = false.obs;
+    sosialMedia = false.obs;
+
+    dashboardEm = true.obs;
+    tambahAkunEm = true.obs;
+    statusPeduliLingkunganEm = true.obs;
   }
 
   void accessAsCitizen() {}
--- lib/modules/estate_manager/screens/menu_folder_screens_em.dart
+++ lib/modules/estate_manager/screens/menu_folder_screens_em.dart
@@ -1,9 +1,11 @@
+import 'package:aplikasi_rw/modules/authentication/controllers/access_controller.dart';
 import 'package:aplikasi_rw/modules/estate_manager/screens/create_account.dart';
 import 'package:aplikasi_rw/modules/estate_manager/screens/dashboard.dart';
 import 'package:aplikasi_rw/modules/home/widgets/menu.dart';
 import 'package:aplikasi_rw/modules/report_screen/screens/add_complaint.dart';
 import 'package:aplikasi_rw/modules/report_screen/screens/sub_menu_report.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
@@ -17,70 +19,107 @@ class MenuFolderEm extends StatefulWidget {
 }
 
 class _MenuFolderEmState extends State<MenuFolderEm> {
+  final accessController = Get.find<AccessController>();
+
   @override
   Widget build(BuildContext context) {
     ScreenUtil.init(context, designSize: const Size(360, 800));
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              HeaderScreen(
-                isEmOrCord: true,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 16.w),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Menu(
-                          icon:
-                              'assets/img/estate_manager_menu/dashboard-em.jpg',
-                          onTap: () {
-                            Get.to(
-                              () => DashboardEm(),
-                              transition: Transition.rightToLeft,
-                            );
-                          },
-                          text: 'Dashboard',
-                        ),
-                        SizedBox(
-                          width: 16.w,
-                        ),
-                        Menu(
-                          icon:
-                              'assets/img/estate_manager_menu/add_account.jpg',
-                          onTap: () {
-                            Get.to(
-                              () => CreateAccount(),
-                              transition: Transition.rightToLeft,
-                            );
-                          },
-                          text: 'Tambah Akun',
-                        ),
-                        SizedBox(
-                          width: 16.w,
-                        ),
-                        Menu(
-                          icon:
-                              'assets/img/estate_manager_menu/status_peduli_lingkungan.jpg',
-                          onTap: () {
-                            Get.to(
-                              () => SubMenuReport(
-                                typeStatusPeduliLingkungan: 'em',
-                              ),
-                              transition: Transition.rightToLeft,
-                            );
-                          },
-                          text: 'Status Peduli Lingkungan',
-                        ),
-                      ],
-                    )
-                  ],
+          child: Obx(
+            () => Column(
+              children: [
+                HeaderScreen(
+                  isEmOrCord: true,
                 ),
-              )
-            ],
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 16.w),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          (accessController.dashboardEm.value)
+                              ? Menu(
+                                  icon:
+                                      'assets/img/estate_manager_menu/dashboard-em.jpg',
+                                  onTap: () {
+                                    Get.to(
+                                      () => DashboardEm(),
+                                      transition: Transition.rightToLeft,
+                                    );
+                                  },
+                                  text: 'Dashboard',
+                                )
+                              : Menu(
+                                  icon:
+                                      'assets/img/estate_manager_menu/dashboard-em.jpg',
+                                  onTap: () {
+                                    EasyLoading.showInfo(
+                                        'Fitur ini hanya bisa diakses oleh estate manager',
+                                        dismissOnTap: true);
+                                  },
+                                  text: 'Dashboard',
+                                ),
+                          SizedBox(
+                            width: 16.w,
+                          ),
+                          (accessController.tambahAkunEm.value)
+                              ? Menu(
+                                  icon:
+                                      'assets/img/estate_manager_menu/add_account.jpg',
+                                  onTap: () {
+                                    Get.to(
+                                      () => CreateAccount(),
+                                      transition: Transition.rightToLeft,
+                                    );
+                                  },
+                                  text: 'Tambah Akun',
+                                )
+                              : Menu(
+                                  icon:
+                                      'assets/img/estate_manager_menu/add_account.jpg',
+                                  onTap: () {
+                                    EasyLoading.showInfo(
+                                        'Fitur ini hanya bisa diakses oleh estate manager',
+                                        dismissOnTap: true);
+                                  },
+                                  text: 'Tambah Akun',
+                                ),
+                          SizedBox(
+                            width: 16.w,
+                          ),
+                          (accessController.statusPeduliLingkunganEm.value)
+                              ? Menu(
+                                  icon:
+                                      'assets/img/estate_manager_menu/status_peduli_lingkungan.jpg',
+                                  onTap: () {
+                                    Get.to(
+                                      () => SubMenuReport(
+                                        typeStatusPeduliLingkungan: 'em',
+                                      ),
+                                      transition: Transition.rightToLeft,
+                                    );
+                                  },
+                                  text: 'Status Peduli Lingkungan',
+                                )
+                              : Menu(
+                                  icon:
+                                      'assets/img/estate_manager_menu/status_peduli_lingkungan.jpg',
+                                  onTap: () {
+                                    EasyLoading.showInfo(
+                                        'Fitur ini hanya bisa diakses oleh estate manager',
+                                        dismissOnTap: true);
+                                  },
+                                  text: 'Status Peduli Lingkungan',
+                                ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
