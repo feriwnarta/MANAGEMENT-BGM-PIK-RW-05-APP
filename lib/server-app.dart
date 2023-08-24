40ce1f86feed647bcd5aef63c007ff3a49e9e4c5
feriwnarta <feriwnarta26@gmail.com>
2023-08-24 10:21:22 +0700

    update

--- lib/server-app.dart
+++ lib/server-app.dart
@@ -1,6 +1,13 @@
 class ServerApp {
+  // server prod
   static String ip = 'next-g.website';
   static String url = 'https://$ip/app/';
+
+  // server test
+  // static String ip = 'next-g.website';
+  // static String url = 'https://$ip/app/';
+
+  // server lokal
   // static String ip = '192.168.3.79';
   // static String url = 'http://$ip/nextg_mobileapp/';
 }
